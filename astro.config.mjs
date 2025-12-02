9b56da0412629357a748961549b722c4d4bfdfb2
lorenachevallot <loni.chevallot@gmail.com>
2025-12-02 23:55:23 +0100

    modifications

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://portfolio.lorena-chevallot.fr",
+
+  integrations: [sitemap()],
+
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      cssMinify: true,
+      minify: "esbuild",
+    },
   },
 
   adapter: node({
-    mode: 'standalone'
+    mode: "standalone",
   }),
-  output: 'server'
+
+  output: "server",
+
+  compressHTML: true,
+
+  image: {
+    domains: ["portfolio.lorena-chevallot.fr"],
+    remotePatterns: [{ protocol: "https" }],
+  },
+
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
 });
